618454e5992eef8df798ea903ec7344608a31ae3
PatatoBit <pathonscript@gmail.com>
2024-11-18 14:36:51 +0700

    initial extension with email sending

---
+++ src/entrypoints/background.ts
@@ -0,0 +1,35 @@
+export default defineBackground(() => {
+  console.log("Hello background!", { id: browser.runtime.id });
+});
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+  Auth,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB6l1sm7V2rmbJMYJQOBMAYxz_vjh1VYhI",
+  authDomain: "backstage-d7299.firebaseapp.com",
+  projectId: "backstage-d7299",
+  storageBucket: "backstage-d7299.firebasestorage.app",
+  messagingSenderId: "128510583025",
+  appId: "1:128510583025:web:a0f6beef79cb0295d54950",
+  measurementId: "G-T5V2ERXL43",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export function signInWithEmailLink(auth: Auth, email: string) {}
---
+++ src/entrypoints/popup/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import EmailAuth from "./../../lib/EmailAuth.svelte";
+  import { auth, signInWithEmailLink } from "../background";
+</script>
+
+<main>
+  <h2>Backstage</h2>
+
+  <EmailAuth />
+</main>
